925c1958429ea9e1ee77438d8e2346c74e89e9eb
Zach <zachery.goodrich@gmail.com>
2020-08-07 18:26:58 -0700

    msg divs are flex again

--- src/components/RoomMessages/RoomMessages.js
+++ src/components/RoomMessages/RoomMessages.js
@@ -20,19 +20,19 @@ const RoomMessages = (props) => {
                     {props.messages.map((val) => {
                         if (val.senderName === props.tempUser.user.username) {
                             return(
-                                <div className='messageDiv'>
+                                <div className='messageDiv outgoingMsgDiv'>
                                     <div className='outgoingMsg'>{`${val.senderName}: ${ReactEmoji.emojify(val.text)}`}</div>
                                 </div>
                             )
                         } else if (val.senderName === 'admin') {
                             return(
-                                <div className='messageDiv'>
+                                <div className='messageDiv adminMsgDiv'>
                                     <div className='adminMsg'>{`${val.senderName}: ${ReactEmoji.emojify(val.text)}`}</div>
                                 </div>
                             )
                         } else {
                             return(
-                                <div className='messageDiv'>
+                                <div className='messageDiv incomingMsgDiv'>
                                     <div className='incomingMsg'>{`${val.senderName}: ${ReactEmoji.emojify(val.text)}`}</div>
                                 </div>
                             )
