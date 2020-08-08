8afb752caa52ff8832136a57a17b47d74c2860de
Zach <zachery.goodrich@gmail.com>
2020-08-08 08:21:55 -0700

    still mongoose

--- src/components/EditUser/EditUser.js
+++ src/components/EditUser/EditUser.js
@@ -8,7 +8,7 @@ const EditUser = (props) => {
 
     const editUser = (e) => {
         e.preventDefault();
-        fetch(`${props.dbUrl}users/${props.tempUser.user.id}`, {
+        fetch(`${props.dbUrl}users/${props.tempUser.user._id}`, {
             body: JSON.stringify({
                 username: username
             }),
